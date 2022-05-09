50b8669338fd8745d59663d8313cf790d4491acb
Przemeks200iq <vertezelopl@gmail.com>
2022-05-09 16:12:10 +0200

    Add first files

---
+++ index.js
@@ -0,0 +1,19 @@
+/*console.log("-----refresh-------");
+
+
+const data = async () => {
+    const response = await fetch(
+        "https://mailcheck.p.rapidapi.com/?domain=vertezelopl@gmail.com",
+        options
+    );
+
+    const data = await response.json();
+    return data;
+};
+
+const start = async () => {
+    const dats = await data();
+    console.log(dats);
+};
+
+start();*/
---
+++ src/apikey.js
@@ -0,0 +1,9 @@
+const OPTIONS = {
+    method: "GET",
+    headers: {
+        "X-RapidAPI-Host": "mailcheck.p.rapidapi.com",
+        "X-RapidAPI-Key": "b08a8cc761msh1160f00fde5354ap18decejsn226e345a8952",
+    },
+};
+
+export default OPTIONS;
