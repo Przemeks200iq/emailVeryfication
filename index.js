ef04cc185a6ecab3c53b6a3c18cc12274c672bc5
Przemeks200iq <vertezelopl@gmail.com>
2022-05-11 20:24:30 +0200

    Add logic and remove sample information

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
 import { connect } from "./src/connent.js";
+const button = document.getElementById("validate");
+const input = document.getElementById("email");
+const inputSpan = document.getElementById("emailValidate");
+const table = document.getElementById("table");
+
+const validate = async () => {
+    const data = await connect(input.value);
+    const { valid } = data;
+    if (valid === false) {
+        table.innerHTML = `
+        <div class="container">
+            <div class="content purple noRight">INFORMATION</div>
+            <div class="content purple">RESULT</div>
+        </div>
+        <div class="container">
+            <div class="content">Valid</div>
+            <div class="content">${valid}</div>
+        </div>
+        `;
+    } else {
+        const { valid, disposable, domain, text, reason } = data;
+        table.innerHTML = `<div class="container">
+                                <div class="content purple noRight">INFORMATION</div>
+                                <div class="content purple">RESULT</div>
+                            </div>
+                            <div class="container">
+                                <div class="content">Valid</div>
+                                <div class="content">${valid}</div>
+                            </div>
+                            <div class="container">
+                                <div class="content">Disposable</div>
+                                <div class="content">${disposable}</div>
+                            </div>
+                            <div class="container">
+                                <div class="content">Domain</div>
+                                <div class="content">${domain}</div>
+                            </div>
+                            <div class="container">
+                                <div class="content">Text</div>
+                                <div class="content">${text}</div>
+                            </div>
+                            <div class="container">
+                                <div class="content">Reason</div>
+                                <div class="content">${reason}</div>
+                            </div>
+        `;
+    }
+};
+
+button.addEventListener("click", validate);
--- src/connent.js
+++ src/connent.js
@@ -1,11 +1,15 @@
 import { OPTIONS } from "./apikey.js";
 
 export const connect = async (domain) => {
-    const response = await fetch(
-        `https://mailcheck.p.rapidapi.com/?domain=${domain}`,
-        OPTIONS
-    );
+    try {
+        const response = await fetch(
+            `https://mailcheck.p.rapidapi.com/?domain=${domain}`,
+            OPTIONS
+        );
 
-    const data = await response.json();
-    return data;
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.log(error);
+    }
 };
